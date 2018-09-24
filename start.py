6ccfdb8c017a64726cdc3afcf5ad9a92fa86d912
Eddie <eddie.ren.2013@gmail.com>
2018-09-24 05:28:15 -0400

    Fixes to startup script

--- start.py
+++ start.py
@@ -9,6 +9,11 @@ snacpshots_check = os.path.join(new, 'pg_snapshots')
 replslot_check = os.path.join(new, 'pg_replslot')
 commit_ts_check = os.path.join(new, 'pg_commit_ts')
 
+logical_mappings = os.path.join(new, 'pg_logical', 'mappings')
+logical_snapshots = os.path.join(new, 'pg_logical', 'snapshots')
+wal_archive = os.path.join(new, 'pg_wal', 'archive_status')
+
+
 if not (os.path.isdir(tblsc_check)):
     os.makedirs(tblsc_check)
 
@@ -26,5 +31,14 @@ if not (os.path.isdir(replslot_check)):
 
 if not (os.path.isdir(commit_ts_check)):
     os.makedirs(commit_ts_check)
+    
+if not (os.path.isdir(logical_mappings)):
+    os.makedirs(logical_mappings)
+    
+if not (os.path.isdir(logical_snapshots)):
+    os.makedirs(logical_snapshots)
+    
+if not (os.path.isdir(wal_archive)):
+    os.makedirs(wal_archive)
 
 print("directories created")
